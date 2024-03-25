185c95aac91aed1175158a6bc3361f1d3c52dad3
Raja Talha <146324114+TALHA-089@users.noreply.github.com>
2024-03-25 08:51:28 -0700

    Update Talha.cpp

--- Talha.cpp
+++ Talha.cpp
@@ -55,144 +55,165 @@ int main()
 
     // Main Program Menu
     vector<Student> students;
-
-
     vector<Teacher> teachers;
-
     vector<Room> rooms;
+    vector<Course> courses;
 
     // Set specific capacity for room "4-19"
-    for (auto& room : rooms) {
-        if (room.code == "4-19") {
+    for (auto& room : rooms) 
+    {
+        if (room.code == "4-19")
+        {
             room.capacity = 16;
             break;
         }
     }
 
-    vector<Course> courses;
-
     int choice;
 
-    do {
+    do 
+    {
         // Display menu
         cout << "\nMenu:\n";
-        cout << "1. View time table by time and day.\n";
-        cout << "2. View time table by day.\n";
-        cout << "3. Add students to rooms.\n";
-        cout << "4. Remove a student from a room.\n";
-        cout << "5. Check room availability for makeup class.\n";
-        cout << "6. Switch teacher's room.\n";
-        cout << "7. Generate teacher-wise time table.\n";
-        cout << "8. Generate section-wise time table.\n";
-        cout << "9. Generate room-wise time table.\n";
-        cout << "10. Add a new course.\n";
-        cout << "0. Exit.\n";
-        cout << "Enter your choice: ";
+        cout << "\n1. View time table by time and day.\n";
+        cout << "\n2. View time table by day.\n";
+        cout << "\n3. Add students to rooms.\n";
+        cout << "\n4. Remove a student from a room.\n";
+        cout << "\n5. Check room availability for makeup class.\n";
+        cout << "\n6. Switch teacher's room.\n";
+        cout << "\n7. Generate teacher-wise time table.\n";
+        cout << "\n8. Generate section-wise time table.\n";
+        cout << "\n9. Generate room-wise time table.\n";
+        cout << "\n10. Add a new course.\n";
+        cout << "\n0. Exit.\n";
+        cout << "\n\nEnter your choice: ";
         cin >> choice;
+        cin.ignore();
 
-        switch (choice) {
-        case 1: {
+        switch (choice) 
+        {
+        case 1: 
+        {
             string time, day;
-            cout << "Enter time (HH:MM): ";
+            cout << "\n\nEnter time (HH:MM): ";
             getline(cin, time);
-            cout << "Enter day: ";
+            cout << "\n\nEnter day: ";
             getline(cin, day);
             displayTimeTableForTime(courses, time, day);
             break;
         }
-        case 2: {
+        case 2: 
+        {
             string day;
-            cout << "Enter day: ";
+            cout << "\n\nEnter day: ";
             getline(cin, day);
             displayTimeTableForDay(courses, day);
             break;
         }
-        case 3: {
+        case 3: 
+        {
             // Adding students to rooms
             string studentName, courseCode, roomNumber;
-            cout << "Enter student name: ";
+            cout << "\n\nEnter student name: ";
             getline(cin, studentName);
-            cout << "Enter course code: ";
+            cout << "\n\nEnter course code: ";
             getline(cin, courseCode);
-            cout << "Enter room number: ";
+            cout << "\n\nEnter room number: ";
             getline(cin, roomNumber);
             // Find the specified course
             Course* selectedCourse = nullptr;
-            for (auto& course : courses) {
-                if (course.code == courseCode && course.room->code == roomNumber) {
+            for (auto& course : courses) 
+            {
+                if (course.code == courseCode && course.room->code == roomNumber)
+                {
                     selectedCourse = &course;
                     break;
                 }
             }
 
-            if (selectedCourse == nullptr) {
-                cout << "Course with code " << courseCode << " is not being taught in room " << roomNumber << "." << endl;
+            if (selectedCourse == nullptr) 
+            {
+                cout << "\n\nCourse with code " << courseCode << " is not being taught in room " << roomNumber << "." << endl;
             }
-            else {
+            else 
+            {
                 // Check if the room has available capacity
-                if (selectedCourse->room->students.size() < selectedCourse->room->capacity) {
+                if (selectedCourse->room->students.size() < selectedCourse->room->capacity) 
+                {
                     // Add the student to the room
                     Student newStudent;
                     newStudent.Name = studentName;
-                    cout << "Enter student roll ID: ";
+                    cout << "\n\nEnter student roll ID: ";
                     cin >> newStudent.ID;
+                    cin.ignore();
                     newStudent.studentSection = ""; // Assuming section information is not entered manually
                     selectedCourse->room->students.push_back(make_shared<Student>(newStudent));
-                    cout << "Student " << studentName << " added to room " << roomNumber << " for course " << selectedCourse->name << "." << endl;
+                    cout << "\n\nStudent " << studentName << " added to room " << roomNumber << " for course " << selectedCourse->name << "." << endl;
                 }
-                else {
-                    cout << "Room " << roomNumber << " is full for course " << selectedCourse->name << "." << endl;
+                else 
+                {
+                    cout << "\n\nRoom " << roomNumber << " is full for course " << selectedCourse->name << "." << endl;
                 }
             }
             break;
         }
-        case 4: {
+        case 4: 
+        {
             string studentName;
-            cout << "Enter student name to remove: ";
+            cout << "\n\nEnter student name to remove: ";
             getline(cin, studentName);
-            for (auto& room : rooms) {
+            for (auto& room : rooms)
+            {
                 removeStudentFromRoom(room, studentName);
             }
-            cout << "Student removed from all rooms successfully.\n";
+            cout << "\n\nStudent removed from all rooms successfully.\n";
             break;
         }
-        case 5: {
+        case 5: 
+        {
             string roomNumber, time;
-            cout << "Enter room number: ";
+            cout << "\n\nEnter room number: ";
             getline(cin, roomNumber);
-            cout << "Enter time (HH:MM): ";
+            cout << "\n\nEnter time (HH:MM): ";
             getline(cin, time);
-            if (isRoomAvailableForMakeupClass(courses, roomNumber, time)) {
-                cout << "Room " << roomNumber << " is available for makeup class at " << time << ".\n";
+            if (isRoomAvailableForMakeupClass(courses, roomNumber, time)) 
+            {
+                cout << "\n\nRoom " << roomNumber << " is available for makeup class at " << time << ".\n";
             }
-            else {
-                cout << "Room " << roomNumber << " is not available for makeup class at " << time << ".\n";
+            else
+            {
+                cout << "\n\nRoom " << roomNumber << " is not available for makeup class at " << time << ".\n";
             }
             break;
         }
-        case 6: {
+        case 6: 
+        {
             string courseCode, newRoomNumber;
-            cout << "Enter the course code of the course to switch: ";
+            cout << "\n\nEnter the course code of the course to switch: ";
             getline(cin, courseCode);
-            cout << "Enter the new room number: ";
+            cout << "\n\nEnter the new room number: ";
             getline(cin, newRoomNumber);
             switchTeacherRoom(courses, courseCode, newRoomNumber, rooms);
-            cout << "Teacher's room switched successfully.\n";
+            cout << "\n\nTeacher's room switched successfully.\n";
             break;
         }
-        case 7: {
+        case 7: 
+        {
             generateTeacherWiseTimeTable(courses);
             break;
         }
-        case 8: {
+        case 8: 
+        {
             generateSectionWiseTimeTable(courses);
             break;
         }
-        case 9: {
+        case 9: 
+        {
             generateRoomWiseTimeTable(courses);
             break;
         }
-        case 10: {
+        case 10:
+        {
             string courseCode, courseName, teacherName, roomNumber, timeSlot, teacherSpecialization;
             cout << "Enter course code: ";
             getline(cin, courseCode);
@@ -212,10 +233,10 @@ int main()
             break;
         }
         case 0:
-            cout << "Exiting...\n";
+            cout << "\nExiting...\n";
             break;
         default:
-            cout << "Invalid choice. Please enter a valid option.\n";
+            cout << "\nInvalid choice. Please enter a valid option.\n";
             break;
         }
 
